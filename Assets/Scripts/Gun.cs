c774a051dd5c6063c71c93a8739add5001ff916b
manuel <manuel.aurora@doconcall.ru>
2018-08-19 20:01:29 +0300

    Add shooting

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -13,10 +13,18 @@ public class Gun : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update() {
-		
+		if (Input.GetMouseButtonDown(0)) {
+			if (!IsInvoking("fireBullet")) {
+				InvokeRepeating("fireBullet", 0f, 0.1f);
+			}
+		}
+
+		if (Input.GetMouseButtonUp(0)) {
+			CancelInvoke();
+		}
 	}
 
-	private void firebullet() {
+	private void fireBullet() {
 		GameObject bullet = Instantiate(bulletPrefab) as GameObject;
 		bullet.transform.position = launchPosition.position;
 		bullet.GetComponent<Rigidbody>().velocity = transform.parent.forward * 400;
